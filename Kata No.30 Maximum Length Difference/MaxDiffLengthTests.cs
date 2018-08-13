0a4738698c01fee5c11582eab85b1db7d44b1ad3
IsaacLu <41774449+IsaacLu@users.noreply.github.com>
2018-08-13 08:51:16 +0800

    Update MaxDiffLengthTests.cs

--- Kata No.30 Maximum Length Difference/MaxDiffLengthTests.cs
+++ Kata No.30 Maximum Length Difference/MaxDiffLengthTests.cs
@@ -9,6 +9,13 @@ public static class MaxDiffLengthTests {
     {
         string[] s1 = new string[]{"hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"};
         string[] s2 = new string[]{"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
+        string[] s3 = new string[]{};
+        string[] s4 = new string[]{"a", "aa", "aaa"};
         Assert.AreEqual(13, MaxDiffLength.Mxdiflg(s1, s2));
+        Assert.AreEqual(-1, MaxDiffLength.Mxdiflg(s1, s3));
+        Assert.AreEqual(10, MaxDiffLength.Mxdiflg(s1, s4));
+        Assert.AreEqual(-1, MaxDiffLength.Mxdiflg(s2, s3));
+        Assert.AreEqual(15, MaxDiffLength.Mxdiflg(s2, s4));
+        Assert.AreEqual(-1, MaxDiffLength.Mxdiflg(s3, s4));
     }
 }
