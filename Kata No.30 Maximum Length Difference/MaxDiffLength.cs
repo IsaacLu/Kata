f16aa844b6d8d9fd98fe817de598c621d62e3446
IsaacLu <41774449+IsaacLu@users.noreply.github.com>
2018-08-13 08:51:32 +0800

    Update MaxDiffLength.cs

--- Kata No.30 Maximum Length Difference/MaxDiffLength.cs
+++ Kata No.30 Maximum Length Difference/MaxDiffLength.cs
@@ -8,48 +8,19 @@ public static class MaxDiffLength
     {
         if (!a1.IsNullOrEmpty() && !a2.IsNullOrEmpty())
         {
-            a2 = FindMinMaxAndDeleteOthers(a2);
-            if (a2.Length == 2)
-                return 13;
-            return -1;
+            int MaxDiff = -1;
+            
+            for (int i = 0; i < a1.Length; i++)
+                for (int j = 0; j < a2.Length; j++)
+                    if (Math.Abs(a1[i].Length - a2[j].Length) > MaxDiff)
+                        MaxDiff = Math.Abs(a1[i].Length - a2[j].Length);
+                
+            return MaxDiff;
         }
          
         return -1;
     }
-    
-    public static string[] FindMinMaxAndDeleteOthers(string[] InputStringArray)
-    {
-        int LongestString = -1, ShortestString = -1;
-        foreach (string InputString in InputStringArray)
-        {
-            if (LongestString == -1 && ShortestString == -1)
-            {
-                LongestString = InputString.Length;
-                ShortestString = InputString.Length;
-            }
-            else if (InputString.Length > LongestString)
-            {
-                InputStringArray = RemoveElement(InputStringArray, InputStringArray[Array.IndexOf(InputStringArray, LongestString)]);
-                LongestString = InputString.Length;
-            }
-            else if (InputString.Length < ShortestString)
-            {
-                InputStringArray = RemoveElement(InputStringArray, InputStringArray[Array.IndexOf(InputStringArray, ShortestString)]);
-                ShortestString = InputString.Length;
-            }
-        }
-        return InputStringArray;
-    }
-    
-    public static string[] RemoveElement(string[] InputStringArray, string StringToRemove)
-    {
-        int StrIdx = Array.IndexOf(InputStringArray, StringToRemove);
-        List<string> tmp = new List<string>(InputStringArray);
-        tmp.RemoveAt(StrIdx);
-        string[] DoneRemovingString = tmp.ToArray();
-        return DoneRemovingString;
-    }
-    
+        
     public static bool IsNullOrEmpty(this Array array)
     {
         return (array == null || array.Length == 0);
