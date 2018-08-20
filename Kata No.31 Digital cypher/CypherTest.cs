84e1263ddfadfe1bda848f608029ab743f4e52f5
IsaacLu <41774449+IsaacLu@users.noreply.github.com>
2018-08-20 21:56:09 +0800

    Update CypherTest.cs

--- Kata No.31 Digital cypher/CypherTest.cs
+++ Kata No.31 Digital cypher/CypherTest.cs
@@ -10,6 +10,7 @@ namespace Learning {
         Assert.AreEqual(new int[] {9, 24, 26}, Kata.Encode("HOW", 1939));
         Assert.AreEqual(new int[] {20, 12, 18, 30, 21}, Kata.Encode("scout", 1939));
         Assert.AreEqual(new int[] {14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8}, Kata.Encode("masterpiece", 1939));
+        Assert.AreEqual(new int[] {20, 6, 22, 29, 10, 19, 23, 14, 8, 12, 10}, Kata.Encode("MaStErPiEcE", 753951));
        
     }
    }
