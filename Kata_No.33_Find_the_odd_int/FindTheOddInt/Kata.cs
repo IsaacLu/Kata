33c6a7ffe37d7810495be461754536c7ed58fdd7
Isaac Lu Li Sin <isaac.lu78@titansoft.com.sg>
2018-09-11 02:40:06 +0800

    Kata No.33

---
+++ Kata_No.33_Find_the_odd_int/FindTheOddInt/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheOddInt
+{
+    public class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            //First edition
+            //int xor = 0;
+
+            //foreach (int Number in seq)
+            //{
+            //    xor ^= Number;
+            //}
+
+            //return xor;
+
+            //Second edition
+            return seq.Aggregate((x, y) => x ^ y);
+        }
+
+    }
+}
---
+++ Kata_No.33_Find_the_odd_int/FindTheOddIntTest/FindTheOddIntTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FindTheOddInt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindTheOddIntTest
+{
+    [TestClass]
+    public class FindTheOddIntTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, Kata.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
