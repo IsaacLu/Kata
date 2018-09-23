5fa03184e781c1df9e776820c39f84e7d4865ee8
Isaac Lu Li Sin <isaac.lu@titansoft.com.sg>
2018-09-24 02:01:13 +0800

    Add No.36

---
+++ Kata No.36 Find The Parity Outlier/FindTheParityOutlier.Test/FindTheParityOutlierTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindTheParityOutlier.Test
+{
+    [TestClass]
+    public class FindTheParityOutlierTests
+    {
+        [TestMethod]
+        public void NegativeValue()
+        {
+            int[] testValue = { 2, 6, 8, -10, 3 };
+            Assert.IsTrue(3 == Kata.Find(testValue));
+        }
+        [TestMethod]
+        public void BiggerValues()
+        {
+            int[] testValue = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+            Assert.IsTrue(206847684 == Kata.Find(testValue));
+        }
+        [TestMethod]
+        public void BiggestValue()
+        {
+            int[] testValue = { int.MaxValue, 0, 1 };
+            Assert.IsTrue(0 == Kata.Find(testValue));
+        }
+    }
+}
---
+++ Kata No.36 Find The Parity Outlier/FindTheParityOutlier/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindTheParityOutlier
+{
+    public class Kata
+    {
+        public static int Find(int[] integers)
+        {
+            return integers.Count(x => (x % 2).Equals(0)) < integers.Count(x => (x % 2).Equals(1))
+                ? integers.FirstOrDefault(x => (x % 2).Equals(0))
+                : integers.FirstOrDefault(x => (x % 2).Equals(1));
+        }
+    }
+}
