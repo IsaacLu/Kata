0006d926a5b753779da190d774612a96fcd8066d
Isaac Lu Li Sin <isaac.lu@titansoft.com.sg>
2018-09-24 01:34:32 +0800

    Set gitignore

--- Kata No.35 Vowel Count/VowelCount/VowelCount.cs
+++ Kata No.35 Vowel Count/VowelCount/VowelCount.cs
@@ -7,6 +7,6 @@ namespace VowelCount
     public static class VowelCount
     {
         private static string Vowel = "aeiou";
-        public static int GetVowelCount(string str) => string.IsNullOrEmpty(str) ? 0 : str.ToLower().Where(z => z.Equals(Vowel.FirstOrDefault(y => y.Equals(z)))).Select(x => x).Count();
+        public static int GetVowelCount(string str) => string.IsNullOrEmpty(str) ? 0 : str.ToLower().Count(x => Vowel.Contains(x));
     }
 }
