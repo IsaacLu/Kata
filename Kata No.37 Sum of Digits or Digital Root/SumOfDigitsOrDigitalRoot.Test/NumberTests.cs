ac4da6845231d71ec50620867f6a918dc0f8dc6a
Isaac Lu Li Sin <isaac.lu@titansoft.com.sg>
2018-09-24 02:33:29 +0800

    Add No.37

---
+++ Kata No.37 Sum of Digits or Digital Root/SumOfDigitsOrDigitalRoot.Test/NumberTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumOfDigitsOrDigitalRoot.Test
+{
+    [TestClass]
+    public class NumberTest
+    {
+        private Number num = new Number();
+        [TestMethod]
+        public void NormalTest() => Assert.AreEqual(7, num.DigitalRoot(16));
+        [TestMethod]
+        public void BiggerNumTest() => Assert.AreEqual(9, num.DigitalRoot(123456789));
+        [TestMethod]
+        public void ZeroTest() => Assert.AreEqual(0, num.DigitalRoot(0));
+    }
+}
---
+++ Kata No.37 Sum of Digits or Digital Root/SumofDigitsoOrDigitalRoot/Number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigitsOrDigitalRoot
+{
+    public class Number
+    {
+        public int DigitalRoot(long n) => n > 10
+            ? DigitalRoot(n.ToString().Select(x => int.Parse(x.ToString())).Sum())
+            : unchecked((int)n);
+    }
+}
